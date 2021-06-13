30d77141bd1132cb1f264fcef6786c5c6534dce5
Flinner <flinner.hlqn@aleeas.com>
2021-06-13 18:01:14 +0300

    init

---
+++ emacs.plugin.zsh
@@ -0,0 +1,120 @@
+#MIT License
+
+#Copyright (c) 2009-2021 Robby Russell and contributors (https://github.com/ohmyzsh/ohmyzsh/contributors)
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+#
+#
+# ==========================================================================================
+# Emacs 23 daemon capability is a killing feature.
+# One emacs process handles all your frames whether
+# you use a frame opened in a terminal via a ssh connection or X frames
+# opened on the same host.
+
+# Benefits are multiple
+# - You don't have the cost of starting Emacs all the time anymore
+# - Opening a file is as fast as Emacs does not have anything else to do.
+# - You can share opened buffered across opened frames.
+# - Configuration changes made at runtime are applied to all frames.
+
+_emacsfun()
+{
+    # get list of emacs frames.
+    frameslist=`emacsclient --alternate-editor '' --eval '(frame-list)' 2>/dev/null | egrep -o '(frame)+'`
+
+    if [ "$(echo "$frameslist" | sed -n '$=')" -ge 2 ] ;then
+        # prevent creating another X frame if there is at least one present.
+        emacsclient --alternate-editor "" "$@"
+    else
+        # Create one if there is no X window yet.
+        emacsclient --alternate-editor "" --create-frame "$@"
+    fi
+}
+
+
+# adopted from https://github.com/davidshepherd7/emacs-read-stdin/blob/master/emacs-read-stdin.sh
+# If the second argument is - then write stdin to a tempfile and open the
+# tempfile. (first argument will be `--no-wait` passed in by the plugin.zsh)
+_emacs_launcher(){
+if [ "$#" -ge "2" -a "$2" = "-" ]
+then
+    tempfile="$(mktemp --tmpdir emacs-stdin-$USERNAME.XXXXXXX 2>/dev/null \
+                || mktemp -t emacs-stdin-$USERNAME)" # support BSD mktemp
+    cat - > "$tempfile"
+    _emacsfun --no-wait $tempfile
+else
+    _emacsfun "$@"
+fi
+}
+
+
+if command -v  emacsclient  2>/dev/null ; then
+    export EMACS_PLUGIN_LAUNCHER=_emacs_launcher
+
+    # set EDITOR if not already defined.
+    export EDITOR="${EDITOR:-${EMACS_PLUGIN_LAUNCHER}}"
+
+    alias emacs="$EMACS_PLUGIN_LAUNCHER --no-wait"
+    alias e=emacs
+    # open terminal emacsclient
+    alias te="$EMACS_PLUGIN_LAUNCHER -nw"
+
+    # same than M-x eval but from outside Emacs.
+    alias eeval="$EMACS_PLUGIN_LAUNCHER --eval"
+    # create a new X frame
+    alias eframe='emacsclient --alternate-editor "" --create-frame'
+
+    # Emacs ANSI Term tracking
+    if [[ -n "$INSIDE_EMACS" ]]; then
+        chpwd_emacs() { print -P "\033AnSiTc %d"; }
+        print -P "\033AnSiTc %d"    # Track current working directory
+        print -P "\033AnSiTu %n"    # Track username        
+
+        # add chpwd hook
+        autoload -Uz add-zsh-hook
+        add-zsh-hook chpwd chpwd_emacs
+    fi    
+
+    # Write to standard output the path to the file
+    # opened in the current buffer.
+    function efile {
+        local cmd="(buffer-file-name (window-buffer))"
+        "$EMACS_PLUGIN_LAUNCHER" --eval "$cmd" | tr -d \"
+    }
+
+    # Write to standard output the directory of the file
+    # opened in the the current buffer
+    function ecd {
+        local cmd="(let ((buf-name (buffer-file-name (window-buffer))))
+                     (if buf-name (file-name-directory buf-name)))"
+
+        local dir="$($EMACS_PLUGIN_LAUNCHER --eval $cmd | tr -d \")"
+        if [ -n "$dir" ] ;then
+            echo "$dir"
+        else
+            echo "can not deduce current buffer filename." >/dev/stderr
+            return 1
+        fi
+    }
+fi
+
+## Local Variables:
+## mode: sh
+## End:
+
